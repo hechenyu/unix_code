b07fff76494e88cf83e6055fde7bef4f1a632db9
hexu <hexu@a.com>
2016-04-19 00:54:20 +0800

    compile wrapunix.c ok

---
+++ unpv13e/lib2/wrapunix.h
@@ -0,0 +1,14 @@
+#ifndef	__unp_wrapunix_h
+#define	__unp_wrapunix_h
+
+#include "error.h"
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/time.h>
+#include <stropts.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/sysctl.h>
+
+#endif
